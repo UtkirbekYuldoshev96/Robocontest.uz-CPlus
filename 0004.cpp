3b2c706508107b9917634ee87a5e973f6452745e
O'tkirbek Yo'ldoshev <91976877+UtkirbekYuldoshev96@users.noreply.github.com>
2022-11-19 00:36:06 +0500

    №0004

---
+++ 0004.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int Galam(int n, int p){
+    float z;
+    z = n * p;
+    cout<< "Siz xaridingiz summasi : "<<z<<" so'm"<<endl;
+}
+
+int main()
+{
+    int a,b;
+    cout<<"Metrdi kiriting: "; cin>>a;
+    cout<<"Galamni narxi kiriting: "; cin>>b;
+    Galam(a,b);
+    return 0;
+}
